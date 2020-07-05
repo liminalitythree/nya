31a5eebe3a94e843f45fbbb15eec54f00155dabe
lain <30793687+liminalitythree@users.noreply.github.com>
2020-07-04 21:51:01 -0600

    idk what i did

--- src/Lib/Parser.fs
+++ src/Lib/Parser.fs
@@ -7,7 +7,7 @@ module Parser =
 
     let ntrue: NyaParser = stringReturn "true" (Atom (Bool true))
     let nfalse: NyaParser = stringReturn "false" (Atom (Bool false))
-
+    
     let nnumber: NyaParser = pfloat |>> (Number >> Atom)
 
     let nidentifier: NyaParser = parse {
@@ -18,3 +18,46 @@ module Parser =
 
     let nstring: NyaParser =
         between (pstring "\"") (pstring "\"") (manySatisfy ((<>) '"')) |>> (String >> Atom)
+
+    let natom =
+        ntrue <|> nfalse <|> nnumber <|> nidentifier <|> nstring
+    
+    let noperator =
+        pstring "+"
+    
+    let ngroup = parse {
+        do! skipString "("
+        let! first = optional nexpr
+        let! rest = many (pstring "," >>. nexpr)
+        do! skipString ")"
+        return Seq (first :: rest)
+    }
+
+    let nlist = parse {
+        do! skipString "["
+        let! first = optional nexpr
+        let! rest = many (pstring "," >>. nexpr)
+        do! skipString "]"
+        return List (first :: rest)
+    }
+
+    let nprimary =
+        natom <|> nlist <|> ngroup
+
+    let nopapply = parse {
+        let! first = nprimary
+        let! rest = many1 (noperator .>>. nprimary)
+        let ops = List.reduce rest (fun x -> x)
+        return Apply ([first])
+    }
+
+    let napply = parse {
+        let! first = nopapply
+        let! rest = many nopapply
+        return Apply (first :: rest)
+    }
+
+    let nexpr = napply
+
+    let nprogram =
+        nexpr .>> eof

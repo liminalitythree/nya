a8dcff0a70ae838dd6cdd923d4e1103072bf04ca
lain <30793687+liminalitythree@users.noreply.github.com>
2020-07-11 17:49:57 -0600

    made the lambda lifter and didnt test it at all

--- src/Lib/Transformers/LambdaLift.fs
+++ src/Lib/Transformers/LambdaLift.fs
@@ -33,7 +33,7 @@ module LambdaLift =
             let self,expr = letrec.E
             getFreeVariables (ignore @ [self]) expr
 
-    let getFreeVarsNoDupes ignore expr = getFreeVariables ignore expr |> Seq.distinct |> Seq.toList
+    let getFreeVarsNoDupes expr = getFreeVariables [] expr |> Seq.distinct |> Seq.toList
 
     type LambdaId = string
 
@@ -44,35 +44,59 @@ module LambdaLift =
         | LList of Infer.A<LNyaExpr list>
         | LApply of Infer.A<LNyaExpr * LNyaExpr>
         | LAtom of Infer.A<NyaAtom>
-        // id * arg type * return type
-        | LLambdaRef of LambdaId * Type.T * Type.T
-        | LLet of Infer.A<string * ANyaExpr>
+        // id * arg types * return type
+        | LLambdaRef of LambdaId * Type.T list * Type.T
+        | LLet of Infer.A<string * LNyaExpr>
         | LLetrec of Type.T * A<string * LNyaExpr>
 
-    // Lambda Id * list of free variable arguments the lambda needs maybe
-    type private LiftedLambda = LambdaId * Infer.A<string> list
+    // Lambda Id, argument to the lambda, list of free variable arguments the lambda needs maybe
+    type LiftedLambda = { Id: string; Args: Infer.A<string> list; Expr: LNyaExpr}
 
     let private mergeMaps map1 map2 = Map.fold (fun acc key value -> Map.add key value acc) map1 map2
 
-    type LambdaLifter () =
-        // map < identifier of lambda,
-        let lifteds = ref Map.empty<string, LiftedLambda>
+    // (hopefully) does lambda lifting
+    // ! NOTE: expects the input expression to only have unique identifiers,
+    // ! eg with Transform.transformUniqueNames
+    // ! maybe
+    let rec lambdaLift (gen: Util.IdGen) (expr: ANyaExpr) : LNyaExpr * Map<LambdaId, LNyaExpr> =
+        let lambdaLift = lambdaLift gen
+        match expr with
+        | AList(_) -> failwith "list is not supported for now maybe maybe maybe"
 
-        let rec lambdaLift (expr: ANyaExpr) : LNyaExpr * Map<LambdaId, LNyaExpr> =
-            match expr with
-            | AList(_) -> failwith "list is not supported for now maybe maybe maybe"
+        | ASeq(seq) ->
+            let l,m = seq.E
+                    |> List.map lambdaLift
+                    |> List.fold (fun (e,emap) (exp,map) -> (e @ [exp]), (mergeMaps emap map)) ([], Map.empty<LambdaId,LNyaExpr>)
 
-            | ASeq(seq) ->
-                let l,m = seq.E
-                        |> List.map lambdaLift
-                        |> List.fold (fun (e,emap) (exp,map) -> (e @ [exp]), (mergeMaps emap map)) ([], Map.empty<LambdaId,LNyaExpr>)
+            ((l |> annotate seq.T |> LSeq), m)
 
-                ((l |> annotate seq.T |> LSeq), m)
+        | AApply(apply) ->
+            let f,x = apply.E
+            let lf,m1 = lambdaLift f
+            let lx,m2 = lambdaLift x
+            (((lf,lx) |> annotate apply.T |> LApply), mergeMaps m1 m2)
 
-            | AApply(apply) ->
-                let f,x = apply.E
-                let lf,m1 = lambdaLift f
-                let lx,m2 = lambdaLift x
-                (((lf,lx) |> annotate apply.T |> LApply), mergeMaps m1 m2)
+        | AAtom(atom) -> (atom |> LAtom), Map.empty
 
-            | AAtom(atom) -> (atom |> LAtom), Map.empty
+        | ALambda(lam) ->
+            let arg,exp = lam.E
+            let args,innerExp = Util.unCurry expr
+
+            let freeVars = (getFreeVarsNoDupes exp)
+                            |> List.map (fun (i,t) -> i |> annotate t)
+            let args = freeVars @ args
+
+            let lInnerExp,lmap = lambdaLift innerExp
+            let lId = gen.Gen()
+            let lref = (lId, (args |> List.map (fun x -> x.T)), lam.T) |> LLambdaRef
+            (lref, lmap.Add(lId, lInnerExp))
+
+        | ALet(lett) ->
+            let i,expr = lett.E
+            let lexpr,lmap = lambdaLift expr
+            ((i,lexpr) |> annotate lett.T |> LLet),lmap
+
+        | ALetrec(t,letrec) ->
+            let i,expr = letrec.E
+            let lexpr,lmap = lambdaLift expr
+            ((t, (i,lexpr) |> annotate letrec.T) |> LLetrec),lmap

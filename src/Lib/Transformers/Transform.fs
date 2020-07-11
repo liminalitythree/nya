88dc1c9ed6cf2377edd36b4aa7248159d034daeb
lain <30793687+liminalitythree@users.noreply.github.com>
2020-07-10 20:53:47 -0600

    added a function to transform identifiers to unique names
    i didnt test it all i hope it works
    maybe

--- src/Lib/Transformers/Transform.fs
+++ src/Lib/Transformers/Transform.fs
@@ -10,5 +10,78 @@
 
 namespace Lib
 
+open Infer
+
 module Transform =
-    let a = 2
+
+    // =================================================================
+    // Renaming all variables to unique names
+
+    type UniqueNameGenerator () =
+        let curid = ref 1
+
+        member this.Gen () =
+            incr curid
+            (!curid - 1).ToString()
+
+    let private genAndAdd (gen: UniqueNameGenerator) (localEnv: Map<string,string> ref) old =
+        let newName = gen.Gen()
+        localEnv := ((!localEnv).Add(old, newName))
+        newName
+
+    let rec transformUniqueNames (localEnv: Map<string, string> ref) (gen: UniqueNameGenerator) (expr: ANyaExpr) : ANyaExpr =
+        match expr with
+        | AList(_) -> failwith "list is not supported for now maybe"
+
+        | ASeq(seq) ->
+            let newEnv = ref localEnv.Value
+            seq.E
+                |> List.map (transformUniqueNames newEnv gen)
+                |> annotate seq.T
+                |> ASeq
+
+        | AAtom(atom) ->
+            match atom.E with
+            | Number(_) | Bool(_) | String(_) -> expr
+
+            | Identifier(ident) ->
+                match (!localEnv).TryFind(ident) with
+                | Some(uniqueName) ->
+                    uniqueName |> Identifier |> annotate atom.T |> AAtom
+
+                | None ->
+                    let newName = genAndAdd gen localEnv ident
+                    newName |> Identifier |> annotate atom.T |> AAtom
+
+        | AApply(apply) ->
+            let f,x = apply.E
+            let uf = transformUniqueNames localEnv gen f
+            let ux = transformUniqueNames localEnv gen x
+
+            (uf, ux) |> annotate apply.T |> AApply
+
+        | ALambda(lam) ->
+            let arg,expr = lam.E
+            let newEnv = ref localEnv.Value
+
+            let newArg = genAndAdd gen newEnv arg.E |> annotate arg.T
+            let uexpr = transformUniqueNames newEnv gen expr
+
+            (newArg, uexpr) |> annotate lam.T |> ALambda
+
+        | ALet(lett) ->
+            let ident,expr = lett.E
+
+            let newIdent = genAndAdd gen localEnv ident
+            let uexpr = transformUniqueNames localEnv gen expr
+
+            (newIdent, uexpr) |> annotate lett.T |> ALet
+
+        | ALetrec(t, letrec) ->
+            let ident,expr = letrec.E
+            let newEnv = ref localEnv.Value
+
+            let newIdent = genAndAdd gen newEnv ident
+            let uexpr = transformUniqueNames newEnv gen expr
+
+            (t, ((newIdent, uexpr) |> annotate letrec.T)) |> ALetrec

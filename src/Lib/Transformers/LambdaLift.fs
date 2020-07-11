575af50e2e4b5a089fc253b40c64b7292ff9d74c
lain <30793687+liminalitythree@users.noreply.github.com>
2020-07-10 23:06:31 -0600

    added getFreeVariables function
    didnt test it :)) i hope it works<>
    maybe
    maybe
    maybe

---
+++ src/Lib/Transformers/LambdaLift.fs
@@ -0,0 +1,34 @@
+namespace Lib
+
+open Infer
+
+module LambdaLift =
+    // note, may have duplicates
+    // ignore - list of variables/identifiers to ignore maybe
+    let rec getFreeVariables (ignore: string list) (expr: ANyaExpr) : string list =
+        match expr with
+        | AList(_) -> failwith "lists are not supported yet i think maybe sorry maybe"
+
+        | ASeq(seq) ->
+            seq.E |> List.fold (fun e x -> e @ getFreeVariables ignore x) []
+
+        | AApply(apply) ->
+            let f,x = apply.E
+            (getFreeVariables ignore f) @ (getFreeVariables ignore x)
+
+        | AAtom(atom) ->
+            match atom.E with
+            | Number(_) | String(_) | Bool(_) -> []
+            | Identifier(ident) -> [ident]
+
+        | ALambda(lam) ->
+            let arg,expr = lam.E
+            getFreeVariables (ignore @ [arg.E]) expr
+
+        | ALet(lett) ->
+            let _,expr = lett.E
+            getFreeVariables ignore expr
+
+        | ALetrec(_, letrec) ->
+            let self,expr = letrec.E
+            getFreeVariables (ignore @ [self]) expr

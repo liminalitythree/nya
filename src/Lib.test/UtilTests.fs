c2b931c32efccecea756b950e29a6a0a6f7f9c2e
lain <30793687+liminalitythree@users.noreply.github.com>
2020-07-11 19:34:28 -0600

    cleaned up unit tests

--- src/Lib.test/UtilTests.fs
+++ src/Lib.test/UtilTests.fs
@@ -3,28 +3,26 @@ module UtilTests
 open System
 open Xunit
 open Xunit.Abstractions
+open FsUnit.Xunit
 open Lib
 open Lib.Infer
 
 type CurryTests(output: ITestOutputHelper) =
+    let args: Infer.A<string> list =
+        seq { 1 .. 3 }
+        |> Seq.fold (fun e x ->
+            e
+            @ [ (("arg" + x.ToString()) |> annotate Type.Num) ]) []
+
+    let expr =
+        2.0
+        |> Number
+        |> annotate Type.Num
+        |> AAtom
 
     [<Fact>]
     member __.``Util.curry works``() =
-        let args: Infer.A<string> list =
-            seq { 1 .. 3 }
-            |> Seq.fold (fun e x ->
-                e
-                @ [ (("arg" + x.ToString()) |> Infer.annotate Type.Num) ]) []
-
-        let expr =
-            2.0
-            |> Number
-            |> Infer.annotate Type.Num
-            |> Infer.AAtom
-
         let res = Util.curry args expr Type.Num
-        output.WriteLine(sprintf "input: %A * %A" args expr)
-        output.WriteLine(sprintf "result: %A" res)
 
         let arg1 = annotate Type.Num "arg1"
         let arg2 = annotate Type.Num "arg2"
@@ -36,32 +34,11 @@ type CurryTests(output: ITestOutputHelper) =
             (arg1, (arg2, (arg3, expr) |> lambdaNum) |> lambdaNum)
             |> lambdaNum
 
-        output.WriteLine(sprintf "expected: %A" expected)
-
-        let a = expected = res
-        Assert.True(a)
+        expected |> should equal res
 
     [<Fact>]
     member __.``Curry and Uncurry``() =
-        let args: Infer.A<string> list =
-            seq { 1 .. 3 }
-            |> Seq.fold (fun e x ->
-                e
-                @ [ (("arg" + x.ToString()) |> Infer.annotate Type.Num) ]) []
-
-        let expr =
-            2.0
-            |> Number
-            |> Infer.annotate Type.Num
-            |> Infer.AAtom
-
-        output.WriteLine(sprintf "original: %A * %A" args expr)
-
         let curried = Util.curry args expr Type.Num
-        output.WriteLine(sprintf "curried: %A" curried)
-
         let unCurried = Util.unCurry curried
-        output.WriteLine(sprintf "unCurried: %A" unCurried)
 
-        let a = unCurried = (args, expr)
-        Assert.True(a)
+        unCurried |> should equal (args, expr)

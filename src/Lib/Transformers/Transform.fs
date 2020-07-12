4334cb3e1a217feec927e7bb092318a581ea9686
lain <30793687+liminalitythree@users.noreply.github.com>
2020-07-11 22:45:57 -0600

    removed done things and things i wont do from TODO

--- src/Lib/Transformers/Transform.fs
+++ src/Lib/Transformers/Transform.fs
@@ -1,10 +1,6 @@
 // TODO:
 // this is just a guess of what would be good
-// move all sequences and lambdas to top level
-// give each variable a unique name (in scopes too) so we can ignore scoping
-//     this might be better to do in the typechecker idk
-// transform curried functions to normal functions (maybe, idk if we need to do this)
-// maybe change constant/non-ref atoms (strings, numbers, bools, etc) to a seperate type
+// move all sequences to top level (idk if this would be useful enough)
 // possibly replace references to variables that are constants with the constant
 // maybe
 

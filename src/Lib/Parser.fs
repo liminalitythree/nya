f6948ff306fdf18b21a8149aab51ad2929fea69b
lain <30793687+liminalitythree@users.noreply.github.com>
2020-07-12 23:49:53 -0600

    Added better comments to parser.fs and organized it a little

--- src/Lib/Parser.fs
+++ src/Lib/Parser.fs
@@ -5,84 +5,7 @@ open FParsec
 module Parser =
     type private NyaParser = Parser<NyaExpr, unit>
 
-    let private ws = spaces
-    let private ws1 = spaces1
-
-    let private strWs s = pstring s .>> ws
-
-    let private nexpr, nexprImpl = createParserForwardedToRef ()
-
-    let private ntrue: NyaParser = stringReturn "true" (Atom(Bool true))
-    let private nfalse: NyaParser = stringReturn "false" (Atom(Bool false))
-
-    let private nnumber: NyaParser = pfloat |>> (Number >> Atom)
-
-    let private nIdentifierStr =
-        let isAsciiIdStart c = isAsciiLetter c || c = '_'
-
-        let isAsciiIdContinue c = isAsciiLetter c || isDigit c || c = '_'
-
-        identifier (IdentifierOptions(isAsciiIdStart = isAsciiIdStart, isAsciiIdContinue = isAsciiIdContinue))
-
-    let private nOpIdentifierStr =
-        let isAsciiIdStart c = c = '@'
-
-        let isAsciiIdContinue c = isAsciiLetter c || isDigit c
-
-        identifier (IdentifierOptions(isAsciiIdStart = isAsciiIdStart, isAsciiIdContinue = isAsciiIdContinue))
-
-    let private nidentifier = nIdentifierStr |>> (Identifier >> Atom)
-
-    let private nstring: NyaParser =
-        between (pstring "\"") (pstring "\"") (manySatisfy ((<>) '"'))
-        |>> (String >> Atom)
-
-    let private nlambdaChar = pstring "\\" <|> pstring "λ"
-
-    let private nlambda =
-        (nlambdaChar >>. nIdentifierStr .>> strWs ".")
-        .>>. nexpr
-        |>> Lambda
-
-    let private natom =
-        ntrue
-        <|> nfalse
-        <|> nnumber
-        <|> nidentifier
-        <|> nstring
-        <|> nlambda
-
-    let private nseq =
-        strWs "("
-        >>. sepBy nexpr (strWs ";")
-        .>> strWs ")"
-        |>> Seq
-
-    let private nlist =
-        strWs "["
-        >>. sepBy nexpr (strWs ",")
-        .>> strWs "]"
-        |>> List
-
-    let private nprimary = (natom <|> nlist <|> nseq) .>> ws
-
-    let private symbolOperators: Parser<string, unit> =
-        strWs "+"
-        <|> strWs "-"
-        <|> strWs "/"
-        <|> strWs "*"
-        <|> strWs "="
-        <|> strWs ":"
-
-    let private noperator =
-        symbolOperators <|> nOpIdentifierStr .>> ws
-
-    // if every element in list is equal
-    // ! no clue if this works
-    let private allEqual (x: 'a list): bool =
-        let first = x.[0]
-        x
-        |> List.fold (fun e x -> (x = first) = e = true) true
+    // ─── UTILITY FUNCTIONS ──────────────────────────────────────────────────────────
 
     let private applyFromList (x: NyaExpr list) =
         let rec afl (x: NyaExpr list) (a: Option<NyaExpr>): NyaExpr =
@@ -101,10 +24,109 @@ module Parser =
 
         afl x None
 
+    // if every element in list is equal
+    // ! no clue if this works
+    let private allEqual (x: 'a list): bool =
+        let first = x.[0]
+        x
+        |> List.fold (fun e x -> (x = first) = e = true) true
+
+    // ─── BUILDING BLOCKS ────────────────────────────────────────────────────────────
+
+    let private ws = spaces
+    let private ws1 = spaces1
+
+    let private strWs s = pstring s .>> ws
+
+    let private nexpr, nexprImpl = createParserForwardedToRef ()
+
+    let private ntrue: NyaParser = stringReturn "true" (Atom(Bool true))
+    let private nfalse: NyaParser = stringReturn "false" (Atom(Bool false))
+
+    let private nnumber: NyaParser = pfloat |>> (Number >> Atom)
+
+    // ─── IDENTIFIERS ────────────────────────────────────────────────────────────────
+
+    let private nIdentifierStr =
+        let isAsciiIdStart c = isAsciiLetter c || c = '_'
+
+        let isAsciiIdContinue c = isAsciiLetter c || isDigit c || c = '_'
+
+        identifier (IdentifierOptions(isAsciiIdStart = isAsciiIdStart, isAsciiIdContinue = isAsciiIdContinue))
+
+    let private nOpIdentifierStr =
+        let isAsciiIdStart c = c = '@'
+
+        let isAsciiIdContinue c = isAsciiLetter c || isDigit c
+
+        identifier (IdentifierOptions(isAsciiIdStart = isAsciiIdStart, isAsciiIdContinue = isAsciiIdContinue))
+
+    let private nidentifier = nIdentifierStr |>> (Identifier >> Atom)
+
+    // ─── STRINGS ────────────────────────────────────────────────────────────────────
+
+    let private nstring: NyaParser =
+        between (pstring "\"") (pstring "\"") (manySatisfy ((<>) '"'))
+        |>> (String >> Atom)
+
+    // ─── LAMBDAS ────────────────────────────────────────────────────────────────────
+
+    let private nlambdaChar = pstring "\\" <|> pstring "λ"
+
+    let private nlambda =
+        (nlambdaChar >>. nIdentifierStr .>> strWs ".")
+        .>>. nexpr
+        |>> Lambda
+
+    // ─── ATOMS ──────────────────────────────────────────────────────────────────────
+
+    let private natom =
+        ntrue
+        <|> nfalse
+        <|> nnumber
+        <|> nidentifier
+        <|> nstring
+        <|> nlambda
+
+    // ─── SEQUENCES ──────────────────────────────────────────────────────────────────
+
+    let private nseq =
+        strWs "("
+        >>. sepBy nexpr (strWs ";")
+        .>> strWs ")"
+        |>> Seq
+
+    // ─── LISTS ──────────────────────────────────────────────────────────────────────
+
+    let private nlist =
+        strWs "["
+        >>. sepBy nexpr (strWs ",")
+        .>> strWs "]"
+        |>> List
+
+    // ─── PRIMARY - FOR OPERATOR PRECEDENCE - ────────────────────────────────────────
+
+    let private nprimary = (natom <|> nlist <|> nseq) .>> ws
+
+    // ─── APPLICATION EXPRESSIONS ────────────────────────────────────────────────────
+
     let private handleNapply (x: NyaExpr list) =
         if x.Length = 1 then x.[0] else applyFromList x
 
     let private napply = many1 nprimary |>> handleNapply
+
+    // ─── OPERATORS ──────────────────────────────────────────────────────────────────
+
+    let private symbolOperators: Parser<string, unit> =
+        strWs "+"
+        <|> strWs "-"
+        <|> strWs "/"
+        <|> strWs "*"
+        <|> strWs "="
+        <|> strWs ":"
+
+    let private noperator =
+        symbolOperators <|> nOpIdentifierStr .>> ws
 
     type private PossibleOpT =
         | Op of string * NyaExpr
@@ -148,6 +170,8 @@ module Parser =
         .>>. many (noperator .>>. napply)
         |>> handleNopapply
 
+    // ─── LET EXPRESSIONS ────────────────────────────────────────────────────────────
+
     let private handleNlet x =
         let a, expr = x
         let isrec, ident = a
@@ -162,6 +186,8 @@ module Parser =
                .>>. nexpr)
               |>> handleNlet))
         <|> nopapply
+
+    // ─── MAIN PARSER ────────────────────────────────────────────────────────────────
 
     do nexprImpl := nlet .>> ws
 

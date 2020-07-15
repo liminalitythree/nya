3a36617a7a105d3a23d6e24337a77760165c27d8
lain <30793687+liminalitythree@users.noreply.github.com>
2020-07-14 19:42:27 -0600

    Update Errors.fs with a type for a pretty-printer error message and deleted useless stuff in it maybe

--- src/Lib/Errors.fs
+++ src/Lib/Errors.fs
@@ -3,15 +3,11 @@ namespace Lib
 module Errors =
     type Pos = FParsec.Position * FParsec.Position
 
-    type LineInfo =
-        { FileName: string
-          Line: int
-          Col: int }
+    // ─── A PRETTY-PRINTED COMPILATON ERROR MAYBE ────────────────────────────────────
 
-    let Error t lineInfo msg =
-        printf "Error at (line: %i, col: %i) in %s:\n%s\n" lineInfo.Line lineInfo.Col lineInfo.FileName msg
-        t
-
-    // wrapper for adding line information and stuff like that
-    // to any type
-    type W<'t> = { T: 't; LineInfo: Option<LineInfo> }
+    type NyaError =
+        {
+          // Error message
+          Msg: string
+          // Position where error occured
+          ErrPos: Pos }
